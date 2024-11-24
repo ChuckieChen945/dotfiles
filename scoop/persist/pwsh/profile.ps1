d6d4a1261546d122dbb3297688c919684f404518
Chuckie Chen <chuckie_chen945@qq.com>
2024-11-24 15:20:00 +0800

    修改配置

--- scoop/persist/pwsh/profile.ps1
+++ scoop/persist/pwsh/profile.ps1
@@ -1,15 +1,9 @@
-
-# enable kmt.winget.autocomplete module
-Import-Module kmt.winget.autocomplete
-
-
 # enable Terminal-Icons module
+# https://github.com/devblackops/Terminal-Icons
 Import-Module -Name Terminal-Icons
-
 
 # enable prediction
 Set-PSReadLineOption -PredictionSource History
-
 
 # enable starship
 Invoke-Expression (&starship init powershell)
